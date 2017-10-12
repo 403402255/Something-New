edea12c9ea66a4ecc9adc1f16f52aa9bcaef6ec2
Tina Ko <tinako1995@gmail.com>
2017-10-12 16:05:50 +0800

    test

--- 3cnav.php
+++ 3cnav.php
@@ -30,7 +30,7 @@
 				<div class="products-right-grids">
 					<div class="sorting">
 						<select id="country" onchange="change_country(this.value)" class="frm-field required sect">
-							<option value="null"><i class="fa fa-arrow-right" aria-hidden="true"></i>Default sorting</option>
+							<option value="null"><i class="fa fa-arrow-right" aria-hidden="true"></i>1Default sorting</option>
 							<option value="null"><i class="fa fa-arrow-right" aria-hidden="true"></i>Sort by popularity</option> 
 							<option value="null"><i class="fa fa-arrow-right" aria-hidden="true"></i>Sort by average rating</option>
 							<option value="null"><i class="fa fa-arrow-right" aria-hidden="true"></i>Sort by date</option>
