f5f3d209e8dc91add48cdc277ba7cf790818f254
wsx98456 <wsx98456@hotmail.com>
2017-10-31 16:51:18 +0800

    1031不要理2

--- config.php
+++ config.php
@@ -3,11 +3,4 @@
 	$username = "User";
 	$password = "12345678";
 	$dbname = "project";
-
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	mysqli_query($conn, 'SET CHARACTER SET utf8');
-
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	} 
 ?>
